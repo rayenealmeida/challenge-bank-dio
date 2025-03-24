2bdf8165e7418557b925a5f07fa12448bb30adb0
rayenealmeida <almeidarayene@gmail.com>
2025-03-24 18:44:03 -0300

    modelo

---
+++ banco.py
@@ -0,0 +1,32 @@
+menu = """
+
+[1] Depositar
+[2] Sacar
+[3] Extrato
+[4] Sair
+
+=> """
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+    opcao = input(menu)
+
+    if opcao == "1":
+        break
+
+    elif opcao == "2":
+        break
+
+    elif opcao == "3":
+        break
+
+    elif opcao == "4":
+        break
+
+    else:
+        print("Operação inválida, por favor selecione a operação correta")
